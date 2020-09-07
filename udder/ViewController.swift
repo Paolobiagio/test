9d5bc41048e403f42b9dab907d27231b29855894
paolobiagio strocchia <paolobiagio.strocchia@gmail.com>
2020-09-07 23:41:20 +0200

    initial commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Paolobiagio  Strocchia on 07/09/2020.
+//  Copyright © 2020 Paolobiagio  Strocchia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
